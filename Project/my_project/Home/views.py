9af565b20dd8a7159f5ec777e8ff93058f895e75
Pulkit-Marlin <pulkitagrawal2000@gmail.com>
2019-11-07 04:20:34 +0530

    Question Bank Database Completed

--- Project/my_project/Home/views.py
+++ Project/my_project/Home/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from . models import Question_Banks_Main
 from .forms import QuestionBankForm, QuestionBankForm2
 
@@ -16,7 +19,7 @@ def add_qb(request):
             qb = form.save(commit=False)
             qb.username = request.user.username
             qb.save()
-            return redirect('qbList')
+            return redirect('Home:qbList')
     else:
         form = QuestionBankForm()
 
@@ -25,9 +28,10 @@ def add_qb(request):
     })
 
 def detail_qb(request, name):
-    qb_detail_list = Question_Banks_Main.objects.filter(username=request.user.username).filter(name=name)
+    qb_detail_list = Question_Banks_Main.objects.filter(name=name)
     return render(request, 'detail_qb.html', {
-        'qb_detail_list': qb_detail_list
+        'qb_detail_list': qb_detail_list,
+        'qb_name': name
     })
 
 def upload_qbfile(request, name):
@@ -38,10 +42,17 @@ def upload_qbfile(request, name):
             qb.username = request.user.username
             qb.name = name
             qb.save()
-            return redirect('detail_qb', name)
+            # base_url = reverse('detail_qb')
+            # url = '{}?{}'.format(base_url, name)
+            # return redirect(url)
+            return redirect('Home:detail_qb',name = name)
+            # return HttpResponse(name)
+            # return redirect('qbList')
     else:
         form = QuestionBankForm2()
 
-    return render(request, 'upload_qb.html', {
+    return render(request, 'add_qb.html', {
         'form': form
     })
+
+    # return HttpResponse(name)
