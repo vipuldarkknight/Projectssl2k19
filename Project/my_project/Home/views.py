44b92ece7391f09de717698a57936d646a667de2
SamarthSingh13 <samsin1372@gmail.com>
2019-11-23 00:48:35 +0530

    changes in parser

--- Project/my_project/Home/views.py
+++ Project/my_project/Home/views.py
@@ -84,12 +84,28 @@ def upload_qbfile(request, name):
             qb.username = request.user.username
             qb.name = name
             qb.save()
+            l=qsplit(qb.file)
+            
+            for x in l:
+                ques_l = Questions_Main()
+                if statement in x.keys():
+                    ques_l.statement = x[statement]
+                if answer in x.keys():
+                    ques_l.answer = x[answer]
+                if marks in x.keys():
+                    ques_l.marks = x[marks]
+                if difficulty in x.keys():
+                    ques_l.difficulty = x[difficulty]
+                if tag in x.keys():
+                    ques_l.tag = x[tag]
             # base_url = reverse('detail_qb')
             # url = '{}?{}'.format(base_url, name)
             # return redirect(url)
-            return redirect('Home:detail_qb',name = name)
+            # return redirect('Home:detail_qb',name = name)
             # return HttpResponse(name)
             # return redirect('qbList')
+            
+            form = QuestionForm(instance=ques_instance)
     else:
         form = QuestionBankForm2()
 
@@ -102,9 +118,9 @@ def delete_qb(request, name):
     Question_Banks_Main.objects.filter(name=name).delete()
     return redirect('Home:qbList')
 
-def qsplit(request):
+def qsplit(qfile):
     con=ConfigParser()
-    con.read(request)
+    con.read(qfile)
     sl=con.sections()
     ql=[]
     for s in sl:
