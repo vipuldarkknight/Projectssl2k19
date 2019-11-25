662d46eec325e0e2a0c58340f32bbc7b82b93108
Pulkit-Marlin <pulkitagrawal2000@gmail.com>
2019-11-25 07:23:36 +0530

    minor changes

--- Project/my_project/Home/views.py
+++ Project/my_project/Home/views.py
@@ -247,7 +247,7 @@ def add_paper(request):
 
                 for i in qlist:
                     id_temp = i
-                    print(id_temp)
+                    # print(id_temp)
                     ids = ids + id_temp + " "
                     ques_temp = Questions_Main.objects.filter(id=id_temp).get()
                     marks_sum = marks_sum + ques_temp.marks
@@ -279,11 +279,11 @@ def add_paper(request):
         q_tup = []
         qm_tup=[]
         for i in q_list:
-            x=[i.id,i.statement[:100]]
+            x=[i.id,""]
             x=tuple(x)
             q_tup.append(x)
         for i in qm_list:
-            x=[i.id,i.statement]
+            x=[i.id,""]
             x=tuple(x)
             qm_tup.append(x)
         q_tup = tuple(q_tup)
@@ -292,8 +292,12 @@ def add_paper(request):
         form.fields["Question_List"].choices = q_tup
         form.fields["Question_Module_List"].choices = qm_tup
 
+        print(form)
+
     return render(request, 'add_paper.html', {
-        'form': form
+        'form': form,
+        'q_list': q_list,
+        'qm_list': qm_list
     })
 
 @login_required
@@ -359,14 +363,27 @@ def edit_ques(request, id):
             ques.username = request.user.username
             ques.qb_name = name
             ques.save()
+
+            papers = created_paper.objects.filter(username=request.user.username)
+            for p in papers:
+                p.marks=0
+                l = p.ques_id.split()
+                l2 = p.ques_module_id.split()
+                for i in l:
+                    p.marks = p.marks + Questions_Main.objects.get(id=i).marks
+                for i in l2:
+                    p.marks = p.marks + Question_Module.objects.get(id=i).marks
+                p.save()
+
             return redirect('Home:detail_qb', name=name)
 
     else:
         form = QuestionForm(instance=ques_instance)
-
         return render(request, 'add_ques_manually.html', {
             'form': form
         })
+
+
 
 @login_required
 def detail_qb(request, name):
@@ -402,7 +419,15 @@ def upload_qbfile(request, name):
             qb = form.save(commit=False)
             qb.username = request.user.username
             qb.name = name
+
+            filepath = request.FILES.get('filepath', False)
+
+            if filepath == False:
+                messages.add_message(request, messages.INFO, 'Some File Must be Uploaded')
+                return redirect('Home:upload_qbfile', name=name)
+
             qb.save()
+
             l=qsplit(qb.file)
             global ques_form_list
             ques_form_list = []
@@ -576,9 +601,31 @@ def edit_subques(request, id):
         form = SubQuestionForm(request.POST, instance=ques_instance)
         if form.is_valid():
             ques = form.save(commit=False)
-            ques.question_module_id = id
             ques.save()
-            return redirect('Home:ques_module_detail', id=id)
+
+            qm_id = ques_instance.question_module_id
+            # print(qm_id)
+            qm = Question_Module.objects.get(id=qm_id)
+
+            sub_id = qm.ques_id_string.split()
+            # sub = SubQuestions.objects.get(id=sub_id)
+            qm.marks = 0
+            for s in sub_id:
+                qm.marks = qm.marks + SubQuestions.objects.get(id=s).marks
+            qm.save()
+
+            papers = created_paper.objects.filter(username=request.user.username)
+            for p in papers:
+                p.marks=0
+                l = p.ques_id.split()
+                l2 = p.ques_module_id.split()
+                for i in l:
+                    p.marks = p.marks + Questions_Main.objects.get(id=i).marks
+                for i in l2:
+                    p.marks = p.marks + Question_Module.objects.get(id=i).marks
+                p.save()
+
+            return redirect('Home:ques_module_detail', id=qm_id)
 
     else:
         form = SubQuestionForm(instance=ques_instance)
